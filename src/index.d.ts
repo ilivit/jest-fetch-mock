bc03ab2a5bd0de421835e1ca0a9338edea0d78e4
Ilya Petukhov <ip@energoservice.net>
2017-11-21 17:52:06 +0300

    update typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,13 @@
-declare interface MockParams {
-  status?: number;
-  statusText?: string;
-  url?: string;
-  headers?: Object;
-}
+/// <reference types="jest" />
+
+type ResponseBody = any;
+type Mock<T> = jest.Mock<Promise<T>>;
 
 declare namespace fetch {
-  function mockResponse(body: string, init?: MockParams): void;
-  function mockResponseOnce(body: string, init?: MockParams): void;
-  function mockResponses(responses: Array<{body: string, init?: MockParams}>): void;
-  function resetMocks(): void;
-  function mockReject(): void;
-  function mockRejectOnce(): void;
+  function mockResponse(body: ResponseBody, init?: ResponseInit): Mock<Response>;
+  function mockResponseOnce(body: ResponseBody, init?: ResponseInit): Mock<Response>;
+  function mockResponses(responses: Array<{ body: ResponseBody, init?: ResponseInit }>): Array<Mock<Response>>;
+  function mockReject(): Mock<never>;
+  function mockRejectOnce(): Mock<never>;
   function resetMocks(): void;
 }
